af7bbe0aee7368d433f31db34412049b20d1716a
Pham Tuan <tuan.pv@ptf.com.vn>
2023-10-25 09:19:00 +0700

    init project

---
+++ server.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const SERVER_PORT = 8000;
+
+server.use(middlewares)
+
+//cors
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
+
+server.use(router)
+
+server.listen(SERVER_PORT, () => {
+    console.log('JSON Server is running on port:', SERVER_PORT)
+})
